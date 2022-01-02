8f1f081559e1f36ddce34a0a5acb39961b14beff
Ahri <chethanawe123@gmail.com>
2022-01-02 14:58:40 +0530

    message

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("connected"));
+app.get("/", (req, res) => {
+  res.send({ message: "hello" });
+});
